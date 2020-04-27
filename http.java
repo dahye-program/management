a904cf0087d7dd4cb3d7beec02b533c4576e9efa
username <dahye0729@sch.ac.kr>
2020-04-28 01:45:40 +0900

    kkk

---
+++ http.java
@@ -0,0 +1,73 @@
+package com.example.capstone_design;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/*
+        WEB 서버와 통신하기 위한 Http 스레드
+*/
+
+public class HttpConnectThread extends Thread {
+    private String m_Url;
+    private String mContent;
+
+    public HttpConnectThread(String url, String Content) {
+        m_Url = url;
+        mContent = Content;
+    }
+    String temp = "";
+
+    // request를 실행하다가 에러가 나면 에러 메세지를 temp안에 담는 함수
+    public void run() {
+        try {
+            temp = request();
+        } catch (Exception e) {
+            temp = e.getMessage();
+        }
+    }
+    public String GetResult() {
+        return temp;
+    }
+    private String request() {
+        StringBuilder output = new StringBuilder();
+        try {
+            URL url = new URL(m_Url);
+
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            if (conn == null) {
+                return "";
+            }
+
+            conn.setConnectTimeout(5000);
+            conn.setRequestMethod("POST"); // URL 요청에 대한 메소드 설정 : POST.
+            conn.setRequestProperty("Accept-Charset", "UTF-8"); // Accept-Charset 설정.
+            conn.setRequestProperty("Context_Type", "application/x-www-form-urlencoded;charset=UTF-8");
+            conn.setDoInput(true);
+            conn.setDoOutput(true);
+
+            OutputStream out = conn.getOutputStream();
+            out.write(mContent.getBytes());
+
+            int resCode = conn.getResponseCode();
+            if (resCode == HttpURLConnection.HTTP_OK) {
+                BufferedReader reader = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+                String line = null;
+                while (true) {
+                    line = reader.readLine();
+                    if (line == null) {
+                        break;
+                    }
+                    output.append(line + "\n");
+                }
+                reader.close();
+                conn.disconnect();
+            }
+        } catch (Exception e) {
+            return e + "";
+        }
+        return output.toString();
+    }
+}
--- test.php
+++ test.php
@@ -1,5 +1,37 @@
+<!doctype html>
+<head><meta charset='utf-8'></head>
 <?php
-    echo "wqehjkqwe";
-    echo "wqhejkejkwqewqe";
-    echo " asdsad";
+    error_reporting(E_ALL);
+    ini_set("display_errors",1);
+        
+    $host='127.0.0.1';  
+    $user='root';   //데이터베이스 이름
+    $pw = ''; //='youn8704';
+    $dbname='mobile_tsicketing';
+    $mysqli = new mysqli($host,$user,$pw,$dbname);
+    
+    if($mysqli){
+        echo "SQL 접근 성공<br/>\n";
+    }   
+    else {
+        echo "SQL 접근 실패<br/>\n";
+    }   
+
+    // app 에서 가입 정보 받아옴
+    $id=$_POST['userid'];
+    $name=$_POST['username'];
+    $password=$_POST['userpasswsord'];
+    $phone=$_POST['userphone'];
+
+    $sql="insert into ticketing_service (name,id,pw,phone)";
+    $sql=$sql."values('$name','$id','$password','$phone')";
+    
+    if($mysqli->query($sql)){
+        echo "inserting 성공";
+        echo "<h3>회원가입 성공</h3>";
+    }
+    else{
+        echo "inserting 실패";
+        echo "<h3>회원가입 실패</h3>";
+    }
 ?>
